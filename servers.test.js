4b8dd618106a92281f12547e0b924209a7f84c39
danielm <dm1905once@gmail.com>
2020-01-21 16:28:07 -0800

    Created servers.test.js

---
+++ servers.test.js
@@ -0,0 +1,27 @@
+describe("Servers test (with setup and tear-down)", function() {
+  beforeEach(function () {
+    // initialization logic
+    serverNameInput.value = 'Alice';
+  });
+
+  it('should add a new server to allServers on submitServerInfo()', function () {
+    submitServerInfo();
+
+    expect(Object.keys(allServers).length).toEqual(1);
+    expect(allServers['server' + serverId].serverName).toEqual('Alice');
+
+  });
+
+  it('should match the contents of allServers with serverTable on updateServerTable()', function(){
+    submitServerInfo();
+    expect(document.querySelector("#serverTable tbody tr").id).toEqual('server1');
+    expect(document.querySelectorAll("#serverTable tbody tr td")[0].innerText).toEqual('Alice');
+    expect(document.querySelectorAll("#serverTable tbody tr td")[1].innerText).toEqual('$0.00');    
+  });
+
+  afterEach(function() {
+    // teardown logic
+    allServers = {};
+    serverTbody.innerHTML = '';
+  });
+});
